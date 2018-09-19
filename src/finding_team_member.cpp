ba95126099f83cf88e718ea8ee1d9228c7199193
Jacopo Notarstefano <jacopo.notarstefano@gmail.com>
2018-09-19 20:10:44 +0200

    src: add solution to finding team member

---
+++ src/finding_team_member.cpp
@@ -0,0 +1,51 @@
+// Copyright (c) 2018 Jacopo Notarstefano
+
+#include <algorithm>
+#include <iostream>
+#include <unordered_map>
+#include <vector>
+
+class Team {
+ public:
+    int i, j, strength;
+
+    explicit Team(int i, int j, int strength): i(i), j(j), strength(strength) {}
+};
+
+std::unordered_map<int, int> finding_team_member(std::vector<Team>& teams) {
+    std::unordered_map<int, int> assignment;
+
+    std::sort(teams.begin(), teams.end(),
+        [](const auto& lhs, const auto& rhs) {
+            return lhs.strength > rhs.strength;
+        });
+
+    for (const auto& team : teams) {
+        bool i_not_found = assignment.find(team.i) == assignment.end(),
+             j_not_found = assignment.find(team.j) == assignment.end();
+        if (i_not_found && j_not_found) {
+            assignment[team.i] = team.j;
+            assignment[team.j] = team.i;
+        }
+    }
+
+    return assignment;
+}
+
+int main() {
+    std::vector<Team> teams;
+
+    int n; std::cin >> n;
+    for (int i = 1; i <= 2 * n; i++) {
+        for (int j = 1; j < i; j++) {
+            int strength; std::cin >> strength;
+            teams.push_back(Team(i, j, strength));
+        }
+    }
+
+    std::unordered_map<int, int> assignment = finding_team_member(teams);
+    for (int i = 1; i <= 2 * n; i++) {
+        std::cout << assignment[i] << ' ';
+    }
+    std::cout << '\n';
+}
