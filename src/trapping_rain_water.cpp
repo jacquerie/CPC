aa19abe6e548c7d4e432336860dd1881730c2ef9
Jacopo Notarstefano <jacopo.notarstefano@gmail.com>
2018-09-20 15:34:31 +0200

    src: document trapping rain water

--- src/trapping_rain_water.cpp
+++ src/trapping_rain_water.cpp
@@ -1,4 +1,21 @@
 // Copyright (c) 2018 Jacopo Notarstefano
+//
+// We do three passes over the array:
+//
+//     1. In the first pass, we write in the i-th element of tallest_left
+//        the height of the tallest tower to the left of the i-th tower.
+//
+//     2. In the second pass, we write in the i-th element of tallest_right
+//        the height of the tallest tower to the right of the i-th tower.
+//
+//     3. In the third and final pass, we compute the result by noting that
+//        the contribution of the i-th element is the difference between the
+//        height of the i-th element and the minimum between tallest_left[i]
+//        and tallest_right[i]. This is because the latter will tell us the
+//        height of the water above the i-th element, while the former will
+//        tell us how much of it is not water.
+//
+// Time: O(n), Space: O(n)
 
 #include <algorithm>
 #include <iostream>
