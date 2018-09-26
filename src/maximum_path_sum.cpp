9287ac10cd4c67b5fee196928f93d547e2393983
Jacopo Notarstefano <jacopo.notarstefano@gmail.com>
2018-09-26 23:03:26 +0200

    src: document maximum path sum

--- src/maximum_path_sum.cpp
+++ src/maximum_path_sum.cpp
@@ -1,4 +1,21 @@
 // Copyright (c) 2018 Jacopo Notarstefano
+//
+// Each child returns to the parent two numbers: the first is the maximum sum
+// of a path ending in the child, the second is the maximum sum of a path in
+// the subtree rooted at the child. Then the parent can recursively compute
+// these two numbers from its children:
+//
+//     1. The maximum sum of a path ending in the parent is the maximum of the
+//        maximum sums of the paths ending in one of the children plus the value
+//        of the parent, unless they are both negative, in which case we discard
+//        both.
+//
+//     2. The maximum sum of a path in the subtree rooted at the parent is the
+//        maximum of the maximum sums of the paths in the subtrees rooted at one
+//        of the children and the sum of the path made by concatenating the
+//        maximum paths ending in the children plus the value of the parent.
+//
+// Time: O(n), Space: O(1)
 
 #include <algorithm>
 #include <climits>
