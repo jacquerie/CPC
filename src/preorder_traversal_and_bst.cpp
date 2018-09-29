c3390ac4d13407ea373a4b0851b1a85ac4d391e0
Jacopo Notarstefano <jacopo.notarstefano@gmail.com>
2018-09-29 15:22:37 +0200

    src: add solution to preorder traversal and bst

---
+++ src/preorder_traversal_and_bst.cpp
@@ -0,0 +1,47 @@
+// Copyright (c) 2018 Jacopo Notarstefano
+
+#include <climits>
+#include <iostream>
+#include <stack>
+#include <vector>
+
+bool preorder_traversal_and_bst(const std::vector<int>& nums) {
+    std::stack<int> stack;
+
+    int current_root = INT_MIN;
+    for (const auto& num : nums) {
+        if (num < current_root) {
+            return false;
+        } else {
+            while (!stack.empty()) {
+                if (num < stack.top()) {
+                    stack.pop();
+                } else {
+                    current_root = stack.top();
+                    break;
+                }
+            }
+
+            stack.push(num);
+        }
+    }
+
+    return true;
+}
+
+int main() {
+    int t; std::cin >> t;
+    for (int i = 0; i < t; i++) {
+        std::vector<int> nums;
+
+        int n; std::cin >> n;
+        for (int j = 0; j < n; j++) {
+            int num; std::cin >> num;
+            nums.push_back(num);
+        }
+
+        std::cout << (preorder_traversal_and_bst(nums) ? 1 : 0) << std::endl;
+
+        nums.clear();
+    }
+}
