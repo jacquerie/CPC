d251087a62c6dc630b0bce31ca4d799f8c6c909c
Jacopo Notarstefano <jacopo.notarstefano@gmail.com>
2018-10-30 09:25:55 +0100

    global: add .ycm_extra_conf.py

---
+++ .ycm_extra_conf.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+
+from __future__ import absolute_import, division, print_function
+
+
+def Settings(**kwargs):
+    return {'flags': ['-x', 'c++', '-Wall', '-Wextra', '-Werror']}
